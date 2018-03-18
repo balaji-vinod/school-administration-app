43bc56568a7c84a76d06895bc562a5b1a1f188db
balaji-vinod <balaji.ksrit@gmail.com>
2018-03-18 18:37:58 +0530

    Added period relation to subject

--- src/main/java/com/school/administration/model/Subject.java
+++ src/main/java/com/school/administration/model/Subject.java
@@ -1,11 +1,14 @@
 package com.school.administration.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Subject {
@@ -18,16 +21,20 @@ public class Subject {
 	@ManyToOne
 	@JoinColumn(name="teacher_id")
 	private Teacher teacher;
+	@OneToMany
+	@JoinColumn(name="subject_id")
+	private List<Period> periods;
 
 	public Subject() {
 		super();
 	}
 
-	public Subject(Long subjectId, String subjectName, Teacher teacher) {
+	public Subject(Long subjectId, String subjectName, Teacher teacher, List<Period> periods) {
 		super();
 		this.subjectId = subjectId;
 		this.subjectName = subjectName;
 		this.teacher = teacher;
+		this.periods = periods;
 	}
 
 	public Long getSubjectId() {
@@ -52,6 +59,14 @@ public class Subject {
 
 	public void setTeacher(Teacher teacher) {
 		this.teacher = teacher;
+	}
+
+	public List<Period> getPeriods() {
+		return periods;
+	}
+
+	public void setPeriods(List<Period> periods) {
+		this.periods = periods;
 	}
 
 }
---
+++ src/main/java/com/school/administration/timetable/service/TimeTableService.java
@@ -0,0 +1,10 @@
+package com.school.administration.timetable.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/timetable")
+public class TimeTableService {
+
+}
